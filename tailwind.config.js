5ad91e13143bae05c109cfea41e33b6bb572d91e
ador <rashedulhaque111@gmail.com>
2022-08-18 12:43:40 +0600

    extra commit for a color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'blue': '#0077B6',
         'skyBlue': '#00B4D8',
         'seaBlue': '#90E0EF',
-        'aqua':'#CAF0F8',
+        'aqua': '#CAF0F8',
         'cream': '#fdf6f1'
       },
     }
